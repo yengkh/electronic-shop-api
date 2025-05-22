a58a5abf987eb51a313cf5f7fb388bb5df443a4c
yengkh <channyengcambo@gmail.com>
2025-05-22 23:17:12 +0700

    Change slug router URL

--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -9,11 +9,10 @@ import {
   getCategoryBySlugHandler,
 } from "../controllers/category.controller";
 const router: Router = express.Router();
-// POST create new category
 router.post("/", createCategoryHandler);
 router.get("/", getAllCategoryHandler);
 router.get("/:identifier", getCategoryHandler);
-router.get("/:slug/slug", getCategoryBySlugHandler);
+router.get("/slug/:slug", getCategoryBySlugHandler);
 router.get("/category/by-path", getCategoryByPathHandler);
 router.delete("/:id", deleteContainerByIdHandler);
 router.put("/:id", updateCategoryhandler);
